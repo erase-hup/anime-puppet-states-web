c335ca5c5538d15a3da7039136d9eecaf28b36f4
erase-hup <yasin.emrebas@gmail.com>
2025-06-17 17:54:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -19,4 +20,10 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // GitHub Pages için base path eklendi
+  base: process.env.NODE_ENV === 'production' ? '/anime-puppet-states-web/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'asset'
+  }
 }));
